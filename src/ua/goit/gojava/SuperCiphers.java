d4ad16bccd5196ff6e6a47acfbaa3b1f996a37f9
alex-korneyko <a.korneyko@gmail.com>
2016-04-06 10:46:27 +0300

    Optimization algorithm

--- src/ua/goit/gojava/SuperCiphers.java
+++ src/ua/goit/gojava/SuperCiphers.java
@@ -19,49 +19,29 @@ public class SuperCiphers {
      */
     public static String Caesar(String string, int shift, boolean encryption) {
 
-        if (encryption) {
-            //Алгоритм шифрования
-            char[] charString = string.toCharArray();       //массив символов из входной строки
-            String result = "";                             //для зашифрованной строки
+        //Если encryption==true, то это шифрование, производится сдвиг по алфавиту вправо
+        //если false, то влево.
+        shift = encryption ? shift : shift * (-1);
 
-            //Шифрование
-            for (int i = 0; i < charString.length; i++) {
+        //Алгоритм шифрования
+        char[] charString = string.toCharArray();       //массив символов из входной строки
+        String result = "";                             //для зашифрованной строки
 
-                /*
-                * формула: y = (x + k)MOD ap
-                * где:  y - зашифрованный символ
-                *       x - шифруемый символ
-                *       k - сдвиг относительно шифруемого символа
-                *       ap - мощность алфавита (к-во символов)
-                */
-                charString[i] = (char) ((charString[i] + shift) % ALPHABET_POWER);
+        //Шифрование
+        for (int i = 0; i < charString.length; i++) {
 
-                //Формирование строки с шифрованным текстом
-                result += charString[i];
-            }
-            return result;
+            /*
+            * формула: y = (x + k)MOD ap
+            * где:  y - зашифрованный символ
+            *       x - шифруемый символ
+            *       k - сдвиг относительно шифруемого символа
+            *       ap - мощность алфавита (к-во символов)
+            */
+            charString[i] = (char) ((charString[i] + shift) % ALPHABET_POWER);
 
-        } else {
-            //Алгоритм дешифрования
-            char[] charString = string.toCharArray();       //массив символов из входной строки
-            String result = "";                             //для расшифрованной строки
-
-            //Дешифрование
-            for (int i = 0; i < charString.length; i++) {
-
-                /*
-                * формула: x = (ap + (y - k))MOD ap
-                * где:  y - зашифрованный символ
-                *       x - расшифрованный символ
-                *       k - сдвиг относительно шифруемого символа
-                *       ap - мощность алфавита (к-во символов)
-                */
-                charString[i] = (char) ((charString[i] - shift) % ALPHABET_POWER);
-
-                //Формирование строки с шифрованным текстом
-                result += charString[i];
-            }
-            return result;
+            //Формирование строки с шифрованным текстом
+            result += charString[i];
         }
+        return result;
     }
 }
